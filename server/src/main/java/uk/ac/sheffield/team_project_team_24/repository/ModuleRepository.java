05aef81976c2abcef6553497ccc126971cc2f77a
zhongwanlin2004@gmail.com <wzhong14@sheffield.ac.uk>
2025-11-24 00:34:37 +0000

    add module code query method

--- server/src/main/java/uk/ac/sheffield/team_project_team_24/repository/ModuleRepository.java
+++ server/src/main/java/uk/ac/sheffield/team_project_team_24/repository/ModuleRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import uk.ac.sheffield.team_project_team_24.domain.module.Module;
 
-public interface ModuleRepository extends JpaRepository<Module, String> {
+import java.util.Optional;
 
+public interface ModuleRepository extends JpaRepository<Module, Long> {
+    Optional<Module> findByModuleCode(String moduleCode);
 }
