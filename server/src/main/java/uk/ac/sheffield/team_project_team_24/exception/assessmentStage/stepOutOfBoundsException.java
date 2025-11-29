d68b5c309aaa940712d74e700516125120bbb300
Cheng An Wong <cawong2@sheffield.ac.uk>
2025-11-29 03:06:00 +0000

    Added useful functions for AssessmentStage

---
+++ server/src/main/java/uk/ac/sheffield/team_project_team_24/exception/assessmentStage/stepOutOfBoundsException.java
@@ -0,0 +1,7 @@
+package uk.ac.sheffield.team_project_team_24.exception.assessmentStage;
+
+public class stepOutOfBoundsException extends RuntimeException {
+    public stepOutOfBoundsException(String message) {
+        super(message);
+    }
+}
