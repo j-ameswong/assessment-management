4c2180809dd21eb7fda582e2296708b4a3a375f4
zhongwanlin2004@gmail.com <wzhong14@sheffield.ac.uk>
2025-12-01 00:29:36 +0000

    implement add-assessment page and connect with backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login.jsx";
 import Home from "./pages/Home.jsx";
+import CreateAssessment from "./pages/CreateAssessment.jsx";
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<Login />} />
             <Route path="/home" element={<Home />} />
+            <Route path="/add-assessment" element={<CreateAssessment />} />
         </Routes>
     );
 }
