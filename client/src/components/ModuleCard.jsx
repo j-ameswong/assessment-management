366f1fbf9b30ec4088ec765a6a653aaac26418c8
RWomack1325 <r.womack1325@gmail.com>
2025-12-02 12:40:48 +0000

    Fixed module dropdown layout issues

--- client/src/components/ModuleCard.jsx
+++ client/src/components/ModuleCard.jsx
@@ -1,33 +1,25 @@
 import React from "react";
-import { useState } from 'react';
+// Dropdown state is now managed by parent
 import { Link } from "react-router-dom";
 import "../componentStyles/ModuleCard.css"
 
-export default function ModuleCard({ moduleCode, moduleTitle }) {
-    const [ddOpen, setDdOpen] = useState(false);
-
-    const toggleDropdown = () => {
-        setDdOpen(prev => !prev)
-    };
-
+export default function ModuleCard({ moduleCode, moduleTitle, isOpen, onToggle }) {
     return (
         <div className="module-box">
             <h2 className="module-code">{moduleCode}</h2>
             <div className="module-info">
                 <div className="module-title-dropdown-row">
                     <h2 className="module-title">{moduleTitle}</h2>
-                    <div className="dropdown">
-                        <button className="dropdown-btn" onClick={toggleDropdown}>
-                            Dropdown
-                        </button>
-                        { ddOpen && (
-                            <div className="dropdown-content">
-                                <Link to="/modules/assessments" className="dropdown-link">See assignments</Link>
-                                <Link to="/modules/assessments" className="dropdown-link">Edit module details</Link>
-                                <Link to="/modules/assessments" className="dropdown-link">Delete modules</Link>
-                            </div>
-                        )}
-                    </div>
+                    <button className="dropdown-btn" onClick={onToggle}>
+                        Dropdown
+                    </button>
+                    { isOpen && (
+                        <div className="dropdown-content">
+                            <Link to="/modules/assessments" className="dropdown-link">See assignments</Link>
+                            <Link to="/modules/assessments" className="dropdown-link">Edit module details</Link>
+                            <Link to="/modules/assessments" className="dropdown-link">Delete module</Link>
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
