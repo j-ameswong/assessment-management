425392cc6beec056610b93d0fd7158ebf13becc8
Connor McGough <cmcgough1@sheffield.ac.uk>
2025-12-09 13:50:57 +0000

    Basic format for user creation page

--- client/src/pages/UserCreation.jsx
+++ client/src/pages/UserCreation.jsx
@@ -11,7 +11,49 @@ return (
   <>
     <Navbar left="COM2008 Systems Design and Security" right="Exam officer"></Navbar>
     {auth ? (
-      <p>Access granted {userRole}</p>
+      <div className="login-page">
+        <div className="login-card">
+          <section className="login-left">
+            <div className="login-left-overlay">
+              <div className="login-left-header">
+              <h2>Create new user</h2>
+              </div>
+              <div className="login-left-logo">
+                  <div className="login-left-logo-placeholder">
+                </div>
+                <p className="login-left-uni">University of Sheffield</p>
+            </div>
+            </div>
+          </section>
+
+          <section className="login-right">
+            <div className="login-right-inner">
+              <h1 className="login-title">New user details</h1>
+              <form className="login-form">
+                <div className="login-field">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    id="email"
+                    type="email"
+                    className="login-input"
+                    placeholder="name@sheffield.ac.uk"
+                  />
+                </div>
+
+                <div className="login-field">
+                  <label htmlFor="password">Password</label>
+                  <input
+                    id="password"
+                    type="password"
+                    className="login-input"
+                  />
+                </div>
+              </form>
+            </div>
+          </section>
+          </div>
+      </div>
+            
     ) : (
       <p>Access denied {userRole}</p>
     )}
