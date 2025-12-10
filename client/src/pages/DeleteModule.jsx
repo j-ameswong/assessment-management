23a9e1b2c55dc48e99bfd1e59824355574a302b9
RWomack1325 <r.womack1325@gmail.com>
2025-12-10 23:36:31 +0000

    Added ability to delete modules, but blocked by referencial integrity

--- client/src/pages/DeleteModule.jsx
+++ client/src/pages/DeleteModule.jsx
@@ -1,14 +1,21 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { useEffect } from "react";
+import axios from "axios";
 
 export default function DeleteModule(){
     const {moduleCode} = useParams();
     const navigate = useNavigate();
 
     const DeleteModule = async () => {
-        const response = await fetch(`http://localhost:8080/api/modules/delete/${moduleCode}`, {
-            method: "DELETE"
-        })
+        const response = await axios.delete(
+                `http://localhost:8080/api/modules/delete/${moduleCode}`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem("token")}`,
+                        "Content-Type": "application/json"
+                    }
+                }
+            );
 
         if(response.ok){
             navigate("/modules")
